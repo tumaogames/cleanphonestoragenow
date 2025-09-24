7af7e544524da08c4ba489fdbdc61af04e12d5ff
artchel06 <artzolina.hplgamedesign@gmail.com>
2025-09-24 16:28:03 +0800

    initial canvas

--- Assets/Scripts/ButtonPulse.cs
+++ Assets/Scripts/ButtonPulse.cs
@@ -1,13 +1,50 @@
-using DG.Tweening;
 using UnityEngine;
+using DG.Tweening;
 
+[RequireComponent(typeof(RectTransform))]
 public class ButtonPulse : MonoBehaviour
 {
-    [SerializeField] private float scaleAmount;
-    [SerializeField] private float pulseDuration;
+    [Header("Pulse Settings")]
+    [SerializeField] private float scaleMultiplier;   // How much bigger than original
+    [SerializeField] private float pulseDuration;     // Time for one up/down cycle
 
-    void OnEnable()
+    private Vector3 _originalScale;
+    private Tween _pulseTween;
+
+    private void Awake()
     {
-        transform.DOScale(scaleAmount, pulseDuration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        _originalScale = transform.localScale;
+    }
+
+    private void OnEnable()
+    {
+        StartPulse();
+    }
+
+    private void OnDisable()
+    {
+        StopPulse(resetScale: true);
+    }
+
+    public void StartPulse()
+    {
+        // Kill any running tween before starting
+        _pulseTween?.Kill();
+
+        Vector3 targetScale = _originalScale * scaleMultiplier;
+
+        _pulseTween = transform
+            .DOScale(targetScale, pulseDuration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+
+    public void StopPulse(bool resetScale = false)
+    {
+        if (_pulseTween != null && _pulseTween.IsActive())
+            _pulseTween.Kill();
+
+        if (resetScale)
+            transform.localScale = _originalScale;
     }
 }
